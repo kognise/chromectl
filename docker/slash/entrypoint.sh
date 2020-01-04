a947fda76623d42c5f3ef9f849662cf01fd09333
Kognise <felix.mattick@gmail.com>
2020-01-04 12:32:09 -0500

    Xvnc

--- docker/slash/entrypoint.sh
+++ docker/slash/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 IFS='x' read SCREEN_WIDTH SCREEN_HEIGHT <<< "${VNC_SCREEN_SIZE}"
 
-export VNC_SCREEN="${SCREEN_WIDTH}x${SCREEN_HEIGHT}x24"
+export VNC_SCREEN="${SCREEN_WIDTH}x${SCREEN_HEIGHT}"
 export CHROME_WINDOW_SIZE="${SCREEN_WIDTH},${SCREEN_HEIGHT}"
 export CHROME_OPTS="${CHROME_OPTS_OVERRIDE:- --user-data-dir --no-sandbox --window-position=0,0 --force-device-scale-factor=1}"
 
